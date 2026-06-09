f8680860f328e0639a60cef675014ceca7571509
Kilykaa <Strimotkilera@gmail.com>
2026-06-09 04:23:24 +0300

    security: приховано паролі та секретні ключі, додано .env.example (Лаба 5)

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://admin:adminpassword@db:5432/fastapi_db"
-    SECRET_KEY: str = "super_secret_key_change_me_in_production"
+    DATABASE_URL: str 
+    SECRET_KEY: str 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
